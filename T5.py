1f0f60fa90406ed9b0f9d0f548221285f56fc9fd
CrisAkm <crisper1821@gmail.com>
2026-04-19 00:00:02 -0600

    Cambios menores

--- T5.py
+++ T5.py
@@ -1,19 +1,32 @@
 #Pequeño script para manejo de archivos
 print("Bienvenido a tu inventario de herramienta")
 
-newtool = input("Ingresa tu nueva herramienta aqui")
 
-"""Usamos a de append en vez de write, asi evitamos cargar todo en la memoria, lo que haciamos en nuestro anterior
-codigo era leer todo y cargarlo en memoria ram, en la ram agregabamos el siguiente dato, y cuando escribiamos con w, borrabamos
-toda la bdd pero como estaba en ram, se volvia a escribir junto con el nuevo dato, asi que aqui no se cargan los datos a ram
-solo se escribe, asi podemos ahorrar recursos"""
-with open ("bdd.txt", "a") as bdd:
-    bdd.write(f"Nueva herramienta agregada: {newtool} \n")
 
 
 class Taller:
     def __init__(self, nombre):
         self.nombre = nombre
 
-    def add_tool(self, herramienta):
-        pass
+    def add_tool(self):
+
+        newtool = input("Ingresa tu nueva herramienta aqui")
+
+        """Usamos a de append en vez de write, asi evitamos cargar todo en la memoria, lo que haciamos en nuestro anterior
+        codigo era leer todo y cargarlo en memoria ram, en la ram agregabamos el siguiente dato, y cuando escribiamos con w, borrabamos
+        toda la bdd pero como estaba en ram, se volvia a escribir junto con el nuevo dato, asi que aqui no se cargan los datos a ram
+        solo se escribe, asi podemos ahorrar recursos"""
+        with open ("bdd.txt", "a") as bdd:
+            bdd.write(f"Nueva herramienta agregada: {newtool} \n")
+
+
+    def read_tool(self):
+        print("Leyendo toda la base de datos")
+
+        with open ("bdd.txt", "r") as bdd:
+            print(bdd.read())
+
+
+Luis = Taller("Luis")
+
+Luis.read_tool()
