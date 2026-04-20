cb821b250a08dc1424f92054d0aae19d10f398c7
CrisAkm <crisper1821@gmail.com>
2026-04-20 13:53:15 -0600

    Version mejorada, sql select ineficiente

--- mi-app/dashboard.py
+++ mi-app/dashboard.py
@@ -1,53 +1,34 @@
 import sqlite3
 import os
 
-bdd_cliente = "bdd_cliente.json" #Variable suelta, si llegamos a reutilizar la clase en otro programa, habra error
-
 class Cliente:
-    def __init__(self, id, nombre, apaterno, amaterno, telefono, direccion, rfc, nacimiento): #XD, le estamos cargando datos que
-        #no deberian estar ahi, es mas funcional o es funcional en un metodo aparte
-        self.id = id
-        self.nombre = nombre
-        self.apaterno = apaterno
-        self.amaterno = amaterno
-        self.telefono = telefono
-        self.direccion = direccion
-        self.rfc = rfc
-        self.nacimiento = nacimiento
-
-    def readc(self):
-        if os.path.exists(bdd_cliente):
-
-            with open(bdd_cliente, 'r') as archivo:  
-                return json.load(archivo)
-            
-        else:
-            return[]
+    def __init__(self, namedb = "main.db"):
+        self.dbname = namedb
+        self.crear_tabla() #Damos la orden inicial de llamar su propio metoo (por eso el self) para que cree la tabla de una
     
-    def createc(self):
-            
-            nuevo_cliente = {
-            "ID": self.id,
-            "NOMBRE": self.nombre,
-            "Apellido_paterno": self.apaterno,
-            "Apellido_materno": self.amaterno,  #LLEVAN COMAS, COMAAASS, NO SE TE OLVIDE, DEBES HACER UN SEPARADOR
-            "Telefono": self.telefono,
-            "direccion": self.direccion,
-            "rfc": self.rfc,
-            "Fecha de nacimiento": self.nacimiento 
-        }
-            with open (bdd_cliente, 'a') as archivo:  #Rompemos todo el json ya que usamos append, 
-                #literal agregamos dos estructuras al mismo archivo
-                json.dump(nuevo_cliente, archivo, indent=4)
+    def conectar(self):
+        return sqlite3.connect(self.dbname) #Conectamos db
+    
+    def crear_tabla(self):
+        conexion = self.conectar() #Llamamos a nuestro propio metodo (po el self) y hacemos el tunel (la conexion)
+        cursor = conexion.cursor() #ponemos conexion ya que es el camino por donde viajaran, y cursor sera la vagoneta
+        #cursor es un metodo propio de python para conectar cualquie db, sqlite, mysql, o en la nube
 
-            
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Clientes (
+                ID_Cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+                Nombre TEXT,
+                Apellido_paterno TEXT,
+                Apellido_materno TEXT,
+                Telefono TEXT,
+                Direccion TEXT,
+                RFC TEXT UNIQUE,
+                Nacimiento TEXT
+            )
+        ''')#Creamos la tabla con su estructura sql, id que se autoincrementa, rfc unico
+        conexion.commit()#guardamos los datos
+        conexion.close()#Cerramos la conexion
 
-C1 = Cliente(3, "Luis", "Perez", "Molina", 24111111, "Av reforma", 9000, "01/04/01")
-
-C1.createc()
-
-lol = C1.readc()
-
-print(len())
-
-
+    def crear_cliente(self, nombre, apaterno, amaterno, telefono, direccion, rfc, nacimiento):
+        self.nombre = nombre
+    
